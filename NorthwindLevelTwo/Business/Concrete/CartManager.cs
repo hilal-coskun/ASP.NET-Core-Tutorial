9592d3beda5ccf988baf21f713698a1aa986d88b
hilal-coskun <hilal.coskun20@gmail.com>
2021-08-17 21:00:34 +0300

    commit

--- NorthwindLevelTwo/Business/Concrete/CartManager.cs
+++ NorthwindLevelTwo/Business/Concrete/CartManager.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using Entities.DomainModels;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -11,17 +12,26 @@ namespace Business.Concrete
     {
         public void AddToCart(Cart cart, Product product)
         {
-            throw new NotImplementedException();
-        }
-
-        public List<CartLine> List(Cart cart)
-        {
-            throw new NotImplementedException();
+            CartLine cartLine = cart.CartLines.FirstOrDefault(c => c.Product.ProductId == product.ProductId);
+            if(cartLine!= null)
+            {
+                cartLine.Quantity++;
+                return;
+            }
+            else
+            {
+                cart.CartLines.Add(new CartLine { Product = product, Quantity = 1 });
+            }
         }
 
         public void RemoveFromCart(Cart cart, int productId)
         {
-            throw new NotImplementedException();
+            cart.CartLines.Remove(cart.CartLines.FirstOrDefault(c => c.Product.ProductId == productId));
+        }
+
+        public List<CartLine> List(Cart cart)
+        {
+            return cart.CartLines;
         }
     }
 }
---
+++ NorthwindLevelTwo/MvcWebUI/Helpers/ICartSessionHelper.cs
@@ -0,0 +1,15 @@
+﻿using Entities.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWebUI.Helpers
+{
+    public interface ICartSessionHelper
+    {
+        Cart GetCart();
+        void SetCart(Cart cart);
+        void Clear();
+    }
+}
