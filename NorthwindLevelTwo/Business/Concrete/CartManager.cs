ae6ed3fe8845155175529db184ac62501faf89c1
hilal-coskun <hilal.coskun20@gmail.com>
2021-08-17 15:10:19 +0300

    commit

--- NorthwindLevelTwo/Business/Concrete/CartManager.cs
+++ NorthwindLevelTwo/Business/Concrete/CartManager.cs
@@ -1,12 +1,27 @@
 ﻿using Business.Abstract;
+using Entities.Concrete;
+using Entities.DomainModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Business.Concrete
 {
-    public class CartManager:ICartService
+    public class CartManager : ICartService
     {
+        public void AddToCart(Cart cart, Product product)
+        {
+            throw new NotImplementedException();
+        }
 
+        public List<CartLine> List(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromCart(Cart cart, int productId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
